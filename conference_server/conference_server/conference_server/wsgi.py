00750b52fe5fd86f994e3984f2bb831c77b962f2
donil <donil@DESKTOP-EV66SOA.(none)>
2023-02-13 21:32:32 +0300

    delete/add files

--- conference_server/conference_server/conference_server/wsgi.py
+++ conference_server/conference_server/conference_server/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for video_app project.
+WSGI config for conference_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'video_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conference_server.settings')
 
 application = get_wsgi_application()
