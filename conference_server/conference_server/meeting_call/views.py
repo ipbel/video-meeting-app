d0065a3aa7ce50cb958d066d4d3a1efd1be22563
donil <donil@DESKTOP-EV66SOA.(none)>
2023-02-27 19:15:48 +0300

    draft

--- conference_server/conference_server/meeting_call/views.py
+++ conference_server/conference_server/meeting_call/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+import os
+from aiohttp import web
 
 
 # Create your views here.
@@ -6,9 +9,19 @@ def index(request):
     return render(request, 'index.html', {})
 
 
-def meeting(request):
-    return render(request, 'meeting.html', {})
+class ServerConnection:
+
+    def sdpconnection(request):
+        return render(HttpRequest, os.path.join('video-meeting-app/sdp_server', 'server.py'))
+
+    async def meeting(request):
+        content = open(os.path.join("video-meeting-app", "meeting.html"), "r").read()
+        return web.Response(content_type="text/html", text=content)
+
+    async def javascript(request):
+        content = open(os.path.join("video-meeting-app", "client.js"), "r").read()
+        return web.Response(content_type="application/javascript", text=content)
 
 
 def statistics(request):
-    return render(request, 'statistics.html', {})
+    return render(request, os.path.join('/templates', 'statistics.html'), {})
